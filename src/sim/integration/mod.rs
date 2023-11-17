4bc3551ea1c8be90d6021a3f56376ebed47486b7
Tibor Völcker <tiborvoelcker@hotmail.de>
2023-11-17 17:31:06 +0100

    Add `Integrator` trait

---
+++ src/sim/integration/mod.rs
@@ -0,0 +1,14 @@
+/*
+ * Created by Tibor Völcker (tiborvoelcker@hotmail.de) on 14.11.23
+ * Last modified by Tibor Völcker on 17.11.23
+ * Copyright (c) 2023 Tibor Völcker (tiborvoelcker@hotmail.de)
+ */
+use super::System;
+
+mod runge_kutta;
+
+trait Integrator {
+    // Function to integrate the system one time step.
+    // It should set the new state and time.
+    fn step<const D: usize>(&self, system: &mut impl System<D>, stepsize: f32);
+}
