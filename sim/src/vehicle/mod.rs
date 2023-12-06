6ad7cd6bbe11272cccf3034e14b13415eaba1a83
Tibor Völcker <tiborvoelcker@hotmail.de>
2023-12-06 20:34:30 +0100

    Move `vehicle.rs` to its own folder

--- sim/src/vehicle/mod.rs
+++ sim/src/vehicle/mod.rs
@@ -1,5 +1,5 @@
 // Created by Tibor Völcker (tiborvoelcker@hotmail.de) on 22.11.23
-// Last modified by Tibor Völcker on 28.11.23
+// Last modified by Tibor Völcker on 06.12.23
 // Copyright (c) 2023 Tibor Völcker (tiborvoelcker@hotmail.de)
 
 use nalgebra::{vector, Vector3};
