a69b40969324082668588b25d8a437264fb31b17
Tibor Völcker <tiborvoelcker@hotmail.de>
2023-12-13 15:41:50 +0100

    Add units and source to constants and conversions

--- sim/src/utils.rs
+++ sim/src/utils.rs
@@ -2,23 +2,23 @@
 // Last modified by Tibor Völcker on 13.12.23
 // Copyright (c) 2023 Tibor Völcker (tiborvoelcker@hotmail.de)
 
-pub const METER_PER_FOOT: f64 = 12. * 25.4 / 1000.;
-
-// helper constant, as `f64::powi` is not a constant function
+// CONVERSIONS from https://physics.nist.gov/cuu/pdf/sp811.pdf pp. 57-69
+pub const METER_PER_FOOT: f64 = 3.048e-01;
 pub const SQUARE_METER_PER_SQUARE_FOOT: f64 = METER_PER_FOOT * METER_PER_FOOT;
 pub const CUBIC_METER_PER_CUBIC_FOOT: f64 = METER_PER_FOOT * METER_PER_FOOT * METER_PER_FOOT;
-
-pub const KELVIN_PER_RANKIN: f64 = 5. / 9.;
-pub const KILOGRAM_PER_POUND: f64 = 4.5359237e-1;
-pub const KILOGRAM_PER_SLUG: f64 = 14.59390;
+pub const KELVIN_PER_RANKIN: f64 = 1. / 1.8;
+pub const KILOGRAM_PER_SLUG: f64 = 1.459390e+01;
+const KILOGRAM_PER_POUND: f64 = 4.535924e-1;
 pub const PASCAL_PER_PSF: f64 = KILOGRAM_PER_POUND * STD_GRAVITY / SQUARE_METER_PER_SQUARE_FOOT;
 
-pub const STD_GRAVITY: f64 = 9.80665;
-const AIR_MOLECULAR_WEIGHT: f64 = 28.9644;
-const GAS_CONSTANT: f64 = 8.31432e3;
-pub const AIR_KAPPA: f64 = 1.40;
-pub const AIR_GAS_CONSTANT: f64 = GAS_CONSTANT / AIR_MOLECULAR_WEIGHT;
+// CONSTANTS from [1] p. IV-7
+pub const STD_GRAVITY: f64 = 9.80665; // [m / s^2]
+const AIR_MOLECULAR_WEIGHT: f64 = 28.9644; // [g / mol]
+const GAS_CONSTANT: f64 = 8.31432e3; // [J / kmol K]
+pub const AIR_KAPPA: f64 = 1.40; // [-]
+pub const AIR_GAS_CONSTANT: f64 = GAS_CONSTANT / AIR_MOLECULAR_WEIGHT; // [J / kg K]
 
+// MACROS FOR TESTING
 #[macro_export]
 macro_rules! assert_lt {
     ($left: expr, $right: expr) => {
