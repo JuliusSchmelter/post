12b922d361f2020a5b65970b009d4a9f00536d1a
Tibor Völcker <tiborvoelcker@hotmail.de>
2024-02-23 17:42:48 +0100

    Remove dbg statements

--- sim/src/lib.rs
+++ sim/src/lib.rs
@@ -1,5 +1,5 @@
 // Created by Tibor Völcker (tiborvoelcker@hotmail.de) on 12.11.23
-// Last modified by Tibor Völcker on 22.02.24
+// Last modified by Tibor Völcker on 23.02.24
 // Copyright (c) 2023 Tibor Völcker (tiborvoelcker@hotmail.de)
 
 // allow dead code for now, as it's still WIP
@@ -16,9 +16,6 @@ pub use integration::Integrator;
 use nalgebra::{vector, Vector3};
 pub use planet::{Atmosphere, Planet, EARTH_FISHER_1960, EARTH_SMITHSONIAN, EARTH_SPHERICAL};
 use state::{PrimaryState, State};
-use utils::constants::{
-    KILOGRAM_PER_POUND, METER_PER_FOOT, NEWTON_PER_POUND_FORCE, PASCAL_PER_PSF,
-};
 pub use vehicle::Vehicle;
 
 use transformations::Transformations;
@@ -45,11 +42,6 @@ impl Simulation {
     }
 
     fn system(&self, state: &PrimaryState) -> State {
-        dbg!(state.time);
-        dbg!(state.position / METER_PER_FOOT);
-        dbg!(state.velocity / METER_PER_FOOT);
-        dbg!(state.mass / KILOGRAM_PER_POUND);
-
         let attitude = self.vehicle.steer(state.time);
 
         let ib = self.transformations.inertial_to_body(
@@ -58,7 +50,6 @@ impl Simulation {
             attitude.z.to_radians(),
         );
         let state = state.add_attitude(attitude, ib);
-        dbg!(state.attitude);
 
         let pressure = self.planet.pressure(state.position);
         let rel_velocity_inertial = self
@@ -71,25 +62,14 @@ impl Simulation {
         let mach = self.planet.mach_number(state.position, state.velocity);
         let dynamic_pressure = self.planet.dynamic_pressure(state.position, state.velocity);
         let state = state.add_env(pressure, alpha, mach, dynamic_pressure);
-        dbg!(pressure / PASCAL_PER_PSF);
-        dbg!(state.velocity.norm() / METER_PER_FOOT);
-        dbg!(rel_velocity_body.norm() / METER_PER_FOOT);
-        dbg!(alpha.to_degrees());
-        dbg!(mach);
-        dbg!(dynamic_pressure / PASCAL_PER_PSF);
 
         let aero = self.vehicle.aero_force(alpha, mach, dynamic_pressure);
-        dbg!(aero / NEWTON_PER_POUND_FORCE);
 
         let throttle = self.vehicle.auto_throttle(state.mass, pressure, aero);
         let thrust = self.vehicle.thrust_force(throttle, pressure);
         let massflow = self.vehicle.massflow(throttle);
-        dbg!(throttle);
-        dbg!(thrust / NEWTON_PER_POUND_FORCE);
-        dbg!(massflow / KILOGRAM_PER_POUND);
 
         let body_acc = (aero + thrust) / state.mass;
-        dbg!(body_acc / METER_PER_FOOT);
 
         // Intersection would require negative thrust
         if body_acc.norm() > self.vehicle.max_acceleration * 1.001 || throttle.is_nan() {
@@ -97,9 +77,6 @@ impl Simulation {
         }
 
         let gravity = self.planet.gravity(state.position);
-
-        dbg!(state.body_to_inertial.transform_vector(&body_acc) / METER_PER_FOOT);
-        dbg!((state.body_to_inertial.transform_vector(&body_acc) + gravity) / METER_PER_FOOT);
 
         state.add_differentials(
             state.body_to_inertial.transform_vector(&body_acc) + gravity,
@@ -132,16 +109,10 @@ impl Simulation {
 
         let geocentric_lat = f64::atan(k.powi(2) * lat.tan());
 
-        dbg!(geocentric_lat.to_degrees());
-        dbg!(long.to_degrees());
-        dbg!(az.to_degrees());
-
         self.transformations.launch = [geocentric_lat, long, az];
 
         let distance_to_surface =
             self.planet.equatorial_radius / f64::sqrt(1. + (k - 1.) * geocentric_lat.sin().powi(2));
-
-        dbg!(distance_to_surface / METER_PER_FOOT);
 
         self.state.position = distance_to_surface
             * vector![
