891ba83d41b7b4a21761e6ded6a3fd700780512b
Tibor Völcker <tiborvoelcker@hotmail.de>
2024-03-07 02:11:18 +0100

    Implement Simulation

--- sim/src/lib.rs
+++ sim/src/lib.rs
@@ -1,5 +1,5 @@
 // Created by Tibor Völcker (tiborvoelcker@hotmail.de) on 04.03.23
-// Last modified by Tibor Völcker on 06.03.24
+// Last modified by Tibor Völcker on 07.03.24
 // Copyright (c) 2023 Tibor Völcker (tiborvoelcker@hotmail.de)
 
 // allow dead code for now, as it's still WIP
@@ -20,24 +20,37 @@ pub use planet::{Planet, EARTH_FISHER_1960, EARTH_SMITHSONIAN, EARTH_SPHERICAL};
 pub use state::State;
 pub use vehicle::{Engine, Vehicle};
 
-pub struct Simulation {}
+#[derive(Debug, Default)]
+pub struct Simulation {
+    phase_inits: Vec<fn(&mut Phase) -> &Phase>,
+}
 
 impl Simulation {
-    pub fn new(vehicle: Vehicle, planet: Planet, stepsize: f64) -> Self {
-        todo!()
+    pub fn new() -> Self {
+        Self::default()
     }
 
-    pub fn add_phase(
-        &self,
-        init_fn: impl Fn(&mut Phase) -> &Phase,
-        termination_fn: impl Fn(&State) -> f64,
-    ) {
-        todo!()
+    pub fn add_phase(&mut self, init_fn: fn(&mut Phase) -> &Phase) -> &mut Self {
+        self.phase_inits.push(init_fn);
+        self
+    }
+
+    pub fn run(&self) {
+        let mut current_phase = Phase::new();
+        for (i, phase_init) in self.phase_inits.iter().enumerate() {
+            println!("Starting Phase {}", i + 1);
+            current_phase.reset();
+            phase_init(&mut current_phase);
+
+            current_phase.run()
+        }
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use utils::constants::STD_GRAVITY;
+
     use self::{
         example_data::{DATA_POINTS, PITCH_RATES, VEHICLES},
         steering::Axis,
@@ -47,61 +60,66 @@ mod tests {
 
     #[test]
     fn full_test() {
-        let sim = Simulation::new(VEHICLES[0].clone(), EARTH_SPHERICAL, 5.);
-        sim.add_phase(
-            |phase| {
-                phase
-                    .add_atmosphere()
-                    .init_geodetic(28.5, 279.4, 90.)
-                    .init_mass(DATA_POINTS[0].mass)
-            },
-            |s| 15. - s.time,
-        );
-        sim.add_phase(
-            |phase| phase.update_steering(Axis::Pitch, [0., PITCH_RATES[0], 0.]),
-            |s| 25. - s.time,
-        );
-        sim.add_phase(
-            |phase| phase.update_steering(Axis::Pitch, [0., PITCH_RATES[1], 0.]),
-            |s| 40. - s.time,
-        );
-        sim.add_phase(
-            |phase| phase.update_steering(Axis::Pitch, [0., PITCH_RATES[2], 0.]),
-            |s| 60. - s.time,
-        );
-        sim.add_phase(
-            |phase| {
-                phase
-                    .update_steering(Axis::Pitch, [0., PITCH_RATES[3], 0.])
-                    .limit_acceleration(3.0)
-            },
-            |s| 120. - s.time,
-        );
-        sim.add_phase(
-            |phase| phase.update_steering(Axis::Pitch, [0., PITCH_RATES[4], 0.]),
-            |s| 150. - s.time,
-        );
-        sim.add_phase(
-            |phase| phase.update_steering(Axis::Pitch, [0., PITCH_RATES[5], 0.]),
-            |s| s.propellant_mass,
-        );
-        sim.add_phase(
-            |phase| phase.limit_acceleration(f64::INFINITY),
-            |s| 7. - s.time_since_event,
-        );
-        sim.add_phase(
-            |phase| {
-                phase
-                    .update_vehicle(VEHICLES[1].clone())
-                    .update_steering(Axis::Pitch, [0., PITCH_RATES[6], 0.])
-                    .limit_acceleration(3.0)
-            },
-            |s| 100. - s.time_since_event,
-        );
-        sim.add_phase(
-            |phase| phase.update_steering(Axis::Pitch, [0., PITCH_RATES[7], 0.]),
-            |s| 150. - s.time_since_event,
-        );
-        sim.add_phase(|phase| phase, |s| s.propellant_mass);
+        let mut sim = Simulation::new();
+        sim.add_phase(|phase| {
+            phase
+                .add_vehicle(VEHICLES[0].clone())
+                .add_atmosphere()
+                .set_stepsize(5.)
+                .init_launch(28.5, 279.4, 90.)
+                .set_mass(DATA_POINTS[0].mass)
+                .update_termination(|s| 15. - s.time)
+        })
+        .add_phase(|phase| {
+            phase
+                .update_steering(Axis::Pitch, [0., PITCH_RATES[0], 0.])
+                .update_termination(|s| 25. - s.time)
+        })
+        .add_phase(|phase| {
+            phase
+                .update_steering(Axis::Pitch, [0., PITCH_RATES[1], 0.])
+                .update_termination(|s| 40. - s.time)
+        })
+        .add_phase(|phase| {
+            phase
+                .update_steering(Axis::Pitch, [0., PITCH_RATES[2], 0.])
+                .update_termination(|s| 60. - s.time)
+        })
+        .add_phase(|phase| {
+            phase
+                .update_steering(Axis::Pitch, [0., PITCH_RATES[3], 0.])
+                .limit_acceleration(3. * STD_GRAVITY)
+                .update_termination(|s| 120. - s.time)
+        })
+        .add_phase(|phase| {
+            phase
+                .update_steering(Axis::Pitch, [0., PITCH_RATES[4], 0.])
+                .update_termination(|s| 150. - s.time)
+        })
+        .add_phase(|phase| {
+            phase
+                .update_steering(Axis::Pitch, [0., PITCH_RATES[5], 0.])
+                .update_termination(|s| s.propellant_mass)
+        })
+        .add_phase(|phase| {
+            phase
+                .limit_acceleration(f64::INFINITY)
+                .update_termination(|s| 7. - s.time_since_event)
+        })
+        .add_phase(|phase| {
+            phase
+                .add_vehicle(VEHICLES[1].clone())
+                .update_steering(Axis::Pitch, [0., PITCH_RATES[6], 0.])
+                .limit_acceleration(3.0)
+                .update_termination(|s| 100. - s.time_since_event)
+        })
+        .add_phase(|phase| {
+            phase
+                .update_steering(Axis::Pitch, [0., PITCH_RATES[7], 0.])
+                .update_termination(|s| 150. - s.time_since_event)
+        })
+        .add_phase(|phase| phase.update_termination(|s| s.propellant_mass));
+
+        sim.run()
     }
 }
