ef782490c038a07fc59553a08451dfd0951ea2a9
Tibor Völcker <tiborvoelcker@hotmail.de>
2024-03-14 11:37:13 +0100

    Fix bug where pitch rates are input wrong

--- sim/src/lib.rs
+++ sim/src/lib.rs
@@ -1,5 +1,5 @@
 // Created by Tibor Völcker (tiborvoelcker@hotmail.de) on 04.03.23
-// Last modified by Tibor Völcker on 11.03.24
+// Last modified by Tibor Völcker on 14.03.24
 // Copyright (c) 2023 Tibor Völcker (tiborvoelcker@hotmail.de)
 
 // allow dead code for now, as it's still WIP
@@ -61,6 +61,7 @@ mod tests {
     #[test]
     fn full_test() {
         let mut sim = Simulation::new();
+        // 1. Phase
         sim.add_phase(|phase| {
             phase
                 .add_vehicle(VEHICLES[0].clone())
@@ -70,56 +71,66 @@ mod tests {
                 .set_mass(DATA_POINTS[0].mass)
                 .update_termination(|s| 15. - s.time)
         })
+        // 2. Phase
         .add_phase(|phase| {
             phase
-                .update_steering(Axis::Pitch, [0., PITCH_RATES[0], 0.])
+                .update_steering(Axis::Pitch, [PITCH_RATES[0], 0., 0.])
                 .update_termination(|s| 25. - s.time)
         })
+        // 3. Phase
         .add_phase(|phase| {
             phase
-                .update_steering(Axis::Pitch, [0., PITCH_RATES[1], 0.])
+                .update_steering(Axis::Pitch, [PITCH_RATES[1], 0., 0.])
                 .update_termination(|s| 40. - s.time)
         })
+        // 4. Phase
         .add_phase(|phase| {
             phase
-                .update_steering(Axis::Pitch, [0., PITCH_RATES[2], 0.])
+                .update_steering(Axis::Pitch, [PITCH_RATES[2], 0., 0.])
                 .update_termination(|s| 60. - s.time)
         })
+        // 5. Phase
         .add_phase(|phase| {
             phase
-                .update_steering(Axis::Pitch, [0., PITCH_RATES[3], 0.])
+                .update_steering(Axis::Pitch, [PITCH_RATES[3], 0., 0.])
                 .limit_acceleration(3. * STD_GRAVITY)
                 .update_termination(|s| 120. - s.time)
         })
+        // 6. Phase
         .add_phase(|phase| {
             phase
-                .update_steering(Axis::Pitch, [0., PITCH_RATES[4], 0.])
+                .update_steering(Axis::Pitch, [PITCH_RATES[4], 0., 0.])
                 .update_termination(|s| 150. - s.time)
         })
+        // 7. Phase
         .add_phase(|phase| {
             phase
-                .update_steering(Axis::Pitch, [0., PITCH_RATES[5], 0.])
+                .update_steering(Axis::Pitch, [PITCH_RATES[5], 0., 0.])
                 .set_stepsize(10.)
                 .update_termination(|s| s.propellant_mass)
         })
+        // 8. Phase
         .add_phase(|phase| {
             phase
                 .limit_acceleration(f64::INFINITY)
                 .update_termination(|s| 7. - s.time_since_event)
         })
+        // 9. Phase
         .add_phase(|phase| {
             phase
                 .add_vehicle(VEHICLES[1].clone())
-                .update_steering(Axis::Pitch, [0., PITCH_RATES[6], 0.])
+                .update_steering(Axis::Pitch, [PITCH_RATES[6], 0., 0.])
                 .limit_acceleration(3.0)
                 .set_stepsize(20.)
                 .update_termination(|s| 100. - s.time_since_event)
         })
+        // 10. Phase
         .add_phase(|phase| {
             phase
-                .update_steering(Axis::Pitch, [0., PITCH_RATES[7], 0.])
+                .update_steering(Axis::Pitch, [PITCH_RATES[7], 0., 0.])
                 .update_termination(|s| 150. - s.time_since_event)
         })
+        // 11. Phase
         .add_phase(|phase| phase.update_termination(|s| s.propellant_mass));
 
         sim.run()
